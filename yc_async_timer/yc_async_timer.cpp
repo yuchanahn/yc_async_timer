64e31da3140a931f80efcbfdeae20678c33a801c
yuchanahn_new_desktop <37448462+yuchanahn@users.noreply.github.com>
2020-12-23 18:57:43 +0900

    thread -> corutine

--- yc_async_timer/yc_async_timer.cpp
+++ yc_async_timer/yc_async_timer.cpp
@@ -3,17 +3,44 @@
 #include "yc_async_timer.hpp"
 
 
+#include <iostream> 
+#include <future> 
+#include <functional>
+#include <thread>
+#include <coroutine>
+#include <chrono>
 
-int main()
-{
-	bool s = false;
-	yc_timer::timer_event([] { printf("0.001sec...\n"); }, 0.001f);
-	yc_timer::timer_event([] { printf("1sec...\n"); }, 1.f);
-	yc_timer::timer_event([] { printf("2sec...\n"); }, 2.f);
-	yc_timer::timer_event([] { printf("2sec...\n"); }, 2.f);
-	yc_timer::timer_event([&s] { printf("2sec...\n");  s = true; }, 2.f);
-	yc_timer::timer_event([&s] { printf("3sec...\n"); }, 3.f);
+using namespace std;
 
-	while (!s);
-	yc_timer::shutdown_timer();
+
+future<void> async_timer(float n, std::function<void()> f) {
+	float dt = 0;
+	while (true)
+	{
+		int ms = (n * 1000.f);
+		bool flg = false;
+		co_await std::async([&] { 
+			std::this_thread::sleep_for(std::chrono::milliseconds(ms));
+			flg = true;
+		});
+		
+		if (flg)
+		{
+			break;
+		}
+	}
+	co_return f();
+} 
+
+
+int main() { 
+	cout << "Start" << endl; 
+	auto f = async_timer(25.f, [] { printf("25ÃÊ Áö³²\n"); });
+	while (!f._Is_ready()) { 
+		std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+		cout << "Main!" << endl; 
+	} 
+	cout << endl; 
+	return 0; 
 }
+
